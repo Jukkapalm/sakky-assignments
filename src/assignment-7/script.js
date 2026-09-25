0dac578ad84035afab80c61f7534c1c56e48cca2
Jukka Palm <jukka.palm@hotmail.fi>
2026-09-25 14:37:56 +0300

    added bonus task

--- src/assignment-7/script.js
+++ src/assignment-7/script.js
@@ -7,23 +7,42 @@ class product {
 
     // Method
     printProducts() {
-        return `Product name: ${this.name}<br>Product price: ${this.price}`;
+        return `Product name: ${this.name}<br>Product price: ${this.price}<br>`;
     }
 }
+
+const productList = [];
 
 function addProduct() {
 
     // Read product name and price from the input 
     const name = document.getElementById("name").value;
     const price = document.getElementById("price").value;
+
+    // Create new product
     const newProduct = new product(name, price);
 
-    console.log(newProduct.name, newProduct.price);
+    // Add product to the productlist
+    productList.push(newProduct)
 
-    // Object
-    const products = document.getElementById("products");
+    // Update text content
+    let printProductlist = "";
+    for (let i = 0; i < productList.length; i++) {
+        printProductlist += productList[i].printProducts();
+    }
 
-    products.innerHTML = `
-    <p>${newProduct.printProducts()}</p>
-    `;
+    // Update products div
+    document.getElementById("products").innerHTML = printProductlist;
+
+    totalPrice();
+}
+
+// Calculates the total of the products prices
+function totalPrice() {
+    let amount = 0;
+    for (let i = 0; i < productList.length; i++) {
+        amount += parseFloat(productList[i].price);
+    }
+
+    document.getElementById("totalprice").innerHTML = amount;
 }
